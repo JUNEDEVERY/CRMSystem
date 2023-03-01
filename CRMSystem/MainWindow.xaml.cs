6c2471c2a8dd2d99bdd85e1274a5a7dd653a38fc
герасимовна <@105-08>
2023-03-01 18:17:14 +0300

    Добавьте файлы проекта.

---
+++ CRMSystem/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CRMSystem
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Employees employees1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DB.tbe = new Entities();
+            btnEntry.Visibility = Visibility.Collapsed;
+            stackPassword.Visibility = Visibility.Collapsed;
+
+
+        }
+
+        private void StackPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void btnEntry_Click(object sender, RoutedEventArgs e)
+        {
+            // List<Employees> list = new List<Employees>();
+
+
+            //List<Employees> employees = DB.tbe.Employees.ToList();
+            //if (employees. == tbNumber.Text)
+            ////{
+            //    tbCode.Visibility = Visibility.Visible;
+
+            //}
+            //else
+            //{
+            //    MessageBox.Show("нет");
+            //}
+        }
+
+        private void tbNumber_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                List<Employees> list1 = DB.tbe.Employees.Where(x => x.Number == tbNumber.Text).ToList();
+
+                if (!string.IsNullOrEmpty(tbNumber.Text))
+                {
+                    if (list1.Count == 1)
+                    {
+                        MessageBox.Show("Номер в базе данных существует");
+                        btnEntry.Visibility = Visibility.Visible;
+                        stackPassword.Visibility = Visibility.Visible;
+                        tbPassword.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Номер отсутствует в базе данных");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Поле номера не заполнено!");
+                }
+
+            }
+        }
+        private void checkedPassword()
+        {
+            if (Regex.IsMatch(tbPassword.Text, @"(?=.[0-9]){1,}"))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("В пароле должна быть хотя бы одна цифра");
+            }
+        }
+
+        private void tbPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(Key.Enter == e.Key)
+            {
+                if (!string.IsNullOrEmpty(tbPassword.Text))
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ CRMSystem/generationCode.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CRMSystem
+{
+    /// <summary>
+    /// Логика взаимодействия для generationCode.xaml
+    /// </summary>
+    public partial class generationCode : Window
+    {
+        public generationCode()
+        {
+            InitializeComponent();
+            Random random = new Random();
+            int kolText = random.Next(8, 8); // Количество символов в строке
+
+            string text = "";
+            for (int i = 0; i < kolText; i++)
+            {
+                int j = random.Next(3); // Выбор 0 - число; 1 - буква
+                if (j == 0)
+                {
+                        text = text + random.Next(9).ToString();
+                }
+                else if (j == 1)
+                {
+                    int l = random.Next(2); // Выбор 0 - заглавная; 1 - маленькая буква; 3 - спецсимвол
+                    if (l == 0)
+                    {
+                        text = text + (char)random.Next('A', 'Z' + 1);
+                    }
+                    else
+                    {
+                        text = text + (char)random.Next('a', 'z' + 1);
+                    }
+                }
+                else if (j == 2)
+                {
+                    string specialSymbol = "/~!@№;%:?*()";
+
+
+                    text += random.Next(0, specialSymbol.Length);
+
+                }
+              
+
+            }
+            tbCode.Text = text;
+
+
+
+        }
+    }
+}
